d655f3c49d19626fdb34083647d88794f257e96a
hspen4 <64763124+hspen4@users.noreply.github.com>
2021-10-23 21:05:54 +1030

    make function

--- calc_initial_evasion.py
+++ calc_initial_evasion.py
@@ -6,20 +6,31 @@ from secml.array import CArray
 from secml_malware.models.malconv import MalConv
 from secml_malware.models.c_classifier_end2end_malware import CClassifierEnd2EndMalware, End2EndModel
 
-# start malconv
+def filter_malware(X,Y,file_names):
 
-net = MalConv()
-net = CClassifierEnd2EndMalware(net)
-net.load_pretrained_model()
+    # start malconv
+
+    net = MalConv()
+    net = CClassifierEnd2EndMalware(net)
+    net.load_pretrained_model()
 
 
-# gets the confidence levels of each input executable initially
+    # gets the confidence levels of each input executable initially
 
-path = "secml_malware/data/malware_samples"
-for _,file in enumerate(os.listdir(path)):
-    loc = os.path.join(path,file)
-    with open(loc,"rb") as opened:
-        contents = opened.read()
-    obj = End2EndModel.bytes_to_numpy(contents,net.get_input_max_length(),256,False)
-    _, conf = net.predict(CArray(obj), True)
-    print(file + " has confidence " + str(conf[0,1].item()))
+    path = "secml_malware/data/malware_samples"
+    for _,file in enumerate(os.listdir(path)):
+        loc = os.path.join(path,file)
+        with open(loc,"rb") as opened:
+            contents = opened.read()
+        obj = End2EndModel.bytes_to_numpy(contents,net.get_input_max_length(),256,False)
+        _, conf = net.predict(CArray(obj), True)
+        print(file + " has confidence " + str(conf[0,1].item()))
+        X.append(obj)
+        confidence = conf[1][0].item()
+        Y.append([1 - confidence, confidence])
+        file_names.append(loc)
+
+X = []
+Y = []
+file_names = []
+filter_malware(X,Y,file_names)
